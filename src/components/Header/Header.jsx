4961ccadb3a728d7a158d87f009c2baf15206901
brunofbpaula <deepcooper@outlook.com>
2023-09-27 02:02:53 -0300

    Create components 'Header' and 'Conteudo'

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+
+export default function Header(){
+
+    return(
+        <>
+        <section className="header-site">
+			<div className="container">
+				<h1 className="text-center">
+					Seja bem-vindo ao Drone Parallax
+				</h1>
+				<p className="text-center lead">
+					Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Non, atque maiores fugit iste ad pariatur adipisci eaque temporibus autem distinctio, dolorum perferendis, similique nesciunt facilis sunt voluptate, modi repellendus dolorem.
+				</p>
+				<p className="text-center">
+					<a href="#" class="btn btn-vermelho">Cadastre-se</a>
+				</p>
+			</div>
+		</section>
+        </>
+    )
+
+}
